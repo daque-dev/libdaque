a0719a73b0f9f159d913325bba80c7cc46006933
David Omar Flores Chávez <davidomarfch@gmail.com>
2018-08-17 03:28:22 -0500

    Change indentation from spaces to tabs

--- source/daque/math/geometry.d
+++ source/daque/math/geometry.d
@@ -1,22 +1,22 @@
 /++
 Authors: 
-    Miguel Ángel (quevangel), quevangel@protonmail.com
-    David Omar Flores Chávez (davidomarf), davidomarfch@gmail.com
+	Miguel Ángel (quevangel), quevangel@protonmail.com
+	David Omar Flores Chávez (davidomarf), davidomarfch@gmail.com
 +/
 module daque.math.geometry;
 
 import std.math;
 
 /++
-    Mathematical dot product
+	Mathematical dot product
 +/
 R dot(R)(R[] v, R[] w)
 in
 {
-    assert(v.length == w.length, 
-        "Dot product can only be applied between to vectors of the same dimension");
-    assert(v.length > 0,
-        "Dot product can't be performed on empty vectors");
+	assert(v.length == w.length, 
+		"Dot product can only be applied between to vectors of the same dimension");
+	assert(v.length > 0,
+		"Dot product can't be performed on empty vectors");
 }
 out
 {
@@ -24,107 +24,107 @@ out
 }
 do
 {
-    import std.array;
-    import std.algorithm;
+	import std.array;
+	import std.algorithm;
 
-    R[] products;
-    products.length = v.length;
-    products[] = v[] * w[];
-    return products.array.sum;
+	R[] products;
+	products.length = v.length;
+	products[] = v[] * w[];
+	return products.array.sum;
 }
 
 ///
 unittest
 {
-    assert(dot([1, 0], [0, 1]) == 0);
-    assert(dot([1, 0], [1, 0]) == 1);
-    assert(dot([1, 2, 3], [1, 2, 3]) == 14);
+	assert(dot([1, 0], [0, 1]) == 0);
+	assert(dot([1, 0], [1, 0]) == 1);
+	assert(dot([1, 2, 3], [1, 2, 3]) == 14);
 }
 
 /++
-    Mathematical distance between to points
+	Mathematical distance between to points
 +/
 real distance(R)(R[] v, R[] w)
 in
 {
-    assert(v.length == w.length, "distance can only be calculated between same-dimensional vectors");
+	assert(v.length == w.length, "distance can only be calculated between same-dimensional vectors");
 }
 out
 {
 }
 do
 {
-    R[] diff;
-    diff.length = v.length;
-    diff[] = w[] - v[];
-    return magnitude(diff);
+	R[] diff;
+	diff.length = v.length;
+	diff[] = w[] - v[];
+	return magnitude(diff);
 }
 
 /++
-    Mathematical cross product
+	Mathematical cross product
 +/
 R[] cross(R)(R[] v, R[] w)
 in
 {
-    assert(v.length == 3 && w.length == 3, "Cross product can only be applied between 3d vectors");
+	assert(v.length == 3 && w.length == 3, "Cross product can only be applied between 3d vectors");
 }
 out (result)
 {
-    R[] resultDup = result.dup;
-    assert(approxEqual(dot(resultDup, v), 0.0) && approxEqual(dot(resultDup, w), 0.0), "Cross product was not orthogonal to it's operands");
+	R[] resultDup = result.dup;
+	assert(approxEqual(dot(resultDup, v), 0.0) && approxEqual(dot(resultDup, w), 0.0), "Cross product was not orthogonal to it's operands");
 }
 do
 {
-    R[] result;
-    result.length = 3;
-    for(uint i; i < 3; i++)
-    {
-        int j = (i + 1) % 3; 
-        int k = (j + 1) % 3;
-        result[i] = v[j] * w[k] - v[k] * w[j];
-    }
-    return result;
+	R[] result;
+	result.length = 3;
+	for(uint i; i < 3; i++)
+	{
+		int j = (i + 1) % 3; 
+		int k = (j + 1) % 3;
+		result[i] = v[j] * w[k] - v[k] * w[j];
+	}
+	return result;
 }
 
 R magnitudeSquared(R)(R[] v)
 out(result)
 {
-    assert(result >= 0);
+	assert(result >= 0);
 }
 do
 {
-    return dot(v, v);
+	return dot(v, v);
 }
 
 real magnitude(R)(R[] v)
 {
-    return sqrt(cast(real)magnitudeSquared(v));
+	return sqrt(cast(real)magnitudeSquared(v));
 }
 
 ///
 unittest{
-    import daque.utils.test;
-    
-    Tester test = new Tester("Magnitude");
+	import daque.utils.test;
+	
+	Tester test = new Tester("Magnitude");
 
-    test.approx!(magnitude!double)([0.0], 0.0);
-    test.approx!(magnitude!double)([1.0, 1.0], sqrt(2.0));
-    test.approx!(magnitude!double)([1.0, 3.0], 0.0);
+	test.approx!(magnitude!double)([0.0], 0.0);
+	test.approx!(magnitude!double)([1.0, 1.0], sqrt(2.0));
+	test.approx!(magnitude!double)([1.0, 3.0], 0.0);
 }
 
 R[] normalize(R)(R[] v)
 in
 {
-    assert(magnitudeSquared(v) != 0, "Cannot normalize a zero vector");
+	assert(magnitudeSquared(v) != 0, "Cannot normalize a zero vector");
 }
 out(result)
 {
-    assert(approxEqual(magnitudeSquared(result.dup), 1), "Normalized vector wasn't unitary");
+	assert(approxEqual(magnitudeSquared(result.dup), 1), "Normalized vector wasn't unitary");
 }
 do
 {
-    R[] normalized;
-    normalized.length = v.length;
-    normalized[] = v[] / magnitude(v);
-    return normalized;
+	R[] normalized;
+	normalized.length = v.length;
+	normalized[] = v[] / magnitude(v);
+	return normalized;
 }
--- source/daque/utils/testlib.d
+++ source/daque/utils/testlib.d
@@ -1,7 +1,7 @@
 /++
 Authors:
-    David Omar Flores Chávez (davidomarf), davidomarf@gmail.com
-    Miguel Ángel (quevangel), quevangel@protonmail.com
+	David Omar Flores Chávez (davidomarf), davidomarf@gmail.com
+	Miguel Ángel (quevangel), quevangel@protonmail.com
 +/
 
 module daque.utils.test;
@@ -14,29 +14,33 @@ import daque.utils.terminal;
 
 class Tester
 {
-    public:
-        this(string functionName)
-        {
-            import std.stdio;
-            writeln("\n--- ", functionName, " test ---");
-        }
-        ~this()
-        {
+	public:
+		this(string functionName)
+		{
+			import std.stdio;
+			writeln("\n--- ", functionName, " test ---");
+		}
+		~this()
+		{
 
-        }
+		}
 
-    void approx(alias func, ReturnType, InputTypes...)
-        (InputTypes inputs, ReturnType expected)
-            {
-                ReturnType result = func(inputs);
-                if(approxEqual(result, expected))
-                {
-                    writeln(cast(string)TerminalColor.L_Green, __traits(identifier, func), "(", inputs, ") = ", expected);
-                }
-                else
-                {
-                    writeln(cast(string)TerminalColor.Red, __traits(identifier, func), "(", inputs, ") = ", result, " != ", expected);
-                }
-                write(cast(string)TerminalColor.NoColor);
-            }
+	void approx(alias func, ReturnType, InputTypes...)
+		(InputTypes inputs, ReturnType expected)
+			{
+				ReturnType result = func(inputs);
+				if(approxEqual(result, expected))
+				{
+					writeln(cast(string)TerminalColor.L_Green,
+						__traits(identifier, func),
+						"(", inputs, ") = ", expected);
+				}
+				else
+				{
+					writeln(cast(string)TerminalColor.Red,
+						__traits(identifier, func),
+						"(", inputs, ") = ", result, " != ", expected);
+				}
+				write(cast(string)TerminalColor.NoColor);
+			}
 }
