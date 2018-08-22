16721131ff7f8a8a6869776adaec02bf8f5ed0ee
Miguel <quevangel@protonmail.com>
2018-08-22 10:36:52 -0500

    Uncomment test

--- source/daque/math/linear.d
+++ source/daque/math/linear.d
@@ -69,14 +69,14 @@ struct Matrix(RealType, uint Rows, uint Columns, MatrixOrder Order = MatrixOrder
         return col;
     }
 
-    R[Rows] applyOn(RealType[] v)
+    RealType[Rows] applyOn(RealType[] v)
     in
     {
         assert(v.length == Columns);
     }
-    out
+    out(result)
     {
-        assert(w.length == Rows);
+        assert(result.length == Rows);
     }
     do
     {
@@ -92,13 +92,13 @@ struct Matrix(RealType, uint Rows, uint Columns, MatrixOrder Order = MatrixOrder
 
 
 
-// unittest
-// {
-// 	import std.algorithm;
-// 	import std.range;
-// 	import std.stdio;
-// 	auto identityColumn = Matrix!(real, 3, 3, MatrixOrder.ColumnMajor).Identity();
-// 	auto identityRow = Matrix!(real, 3, 3, MatrixOrder.RowMajor).Identity();
-// 	writeln(identityColumn.linearize!(MatrixOrder.RowMajor)());
-// 	writeln(identityRow);
-// }
+unittest
+{
+    import std.algorithm;
+    import std.range;
+    import std.stdio;
+    auto identityColumn = Matrix!(real, 3, 3, MatrixOrder.ColumnMajor).Identity();
+    auto identityRow = Matrix!(real, 3, 3, MatrixOrder.RowMajor).Identity();
+    writeln(identityColumn.linearize!(MatrixOrder.RowMajor)());
+    writeln(identityRow);
+}
