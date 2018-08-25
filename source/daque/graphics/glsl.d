3e7adf2a15716ae5159639b87d26f4c5c1742644
Miguel <quevangel@protonmail.com>
2018-08-25 17:43:19 -0500

    GetUniformPostfix

--- source/daque/graphics/glsl.d
+++ source/daque/graphics/glsl.d
@@ -6,3 +6,16 @@ enum Type
     Vec3,
     Mat3
 }
+
+string GetUniformPostfix(Type t)
+{
+    final switch(t)
+    {
+        case Float:
+            return "1fv";
+        case Vec3:
+            return "3fv";
+        case Mat3:
+            return "Matrix3v";
+    }
+}
